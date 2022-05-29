a9e7c4e4ad8d7d009eb0fe2883cac4134d473c91
rcp444 <kar.pergol@gmail.com>
2022-05-29 04:13:51 +0200

    Fixed message communication

--- server.c
+++ server.c
@@ -10,6 +10,11 @@
 #include <sys/syscall.h>
 #include "Message.h"
 
+char* commands[] = 
+{
+	"status",
+	"pingcrc"
+};
 
 void* connection_handler(void *socket_desc) {
 	
@@ -23,30 +28,37 @@ void* connection_handler(void *socket_desc) {
 	memset(client_message, 0, 2000);
 	// free(test_struct_message);
 	Message* received_test_struct = malloc(sizeof(Message));
+	ping_crc* ping_struct = malloc(sizeof(ping_crc));
+
 	do {
 
 		/* Read command part */
 		/* Exisiting commands:
 			-> status
-			->
+			-> pingcrc
 			->
 		*/
 
-		read_size = recv(sock , client_message , 8 , 0);
-		client_message[8] = '\0';
-		//printf("\n%s\n", client_message);
+		read_size = recv(sock , client_message , 20 , 0);
 
-		char command[] = "status";
-		if(strcmp(client_message, command) == 0)
+		client_message[read_size] = '\0';
+		printf("\n[ %d -> %s ]\n", read_size, client_message);
+
+		if(strcmp(client_message, commands[0]) == 0)
 		{
 			printf("Waiting for message\n");
 			read_size = recv(sock, received_test_struct, sizeof(Message), 0);
-			printf("Message read\n");
-			printf("{ %f %f %s }", received_test_struct->x, received_test_struct->y, received_test_struct->message);
-			printf("\n");
-
+			printf("{ %f %f %s \t}", received_test_struct->x, received_test_struct->y, received_test_struct->message);
 			/* Send the message back to client */
-			write(sock, received_test_struct, sizeof(Message));
+			//write(sock, received_test_struct, sizeof(Message));
+		}
+		//printf("%s \n%s", client_message, commands[1]);
+		if(strcmp(client_message, commands[1]) == 0)
+		{
+			read_size = recv(sock, ping_struct, sizeof(ping_crc), 0);
+			/* Send the message back to client */
+			printf("pingcrc {%d}", ping_struct->ping);
+			write(sock, ping_struct, sizeof(ping_crc));
 		}
 		
 		/* Clear the message buffer */
@@ -79,7 +91,7 @@ int main(int argc, char *argv[]) {
 
 	for (;;) {
 		connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
-		fprintf(stderr, "Connection accepted\n"); 
+		fprintf(stderr, "\nConnection accepted\n"); 
 		pthread_create(&thread_id, NULL, connection_handler , (void *) &connfd);
 	}
 }
